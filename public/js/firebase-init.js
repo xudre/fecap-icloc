c3a268b08f2a5d9f8e5a7f1953c29f91816ecad1
Pedro Xudre <pedro@xudre.com>
2019-05-22 10:19:36 -0300

    First setup

---
+++ public/js/firebase-init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCsRKOHn2tvvx69k9EtKUrMAubcguOE68k",
+  authDomain: "icloc-fecap.firebaseapp.com",
+  databaseURL: "https://icloc-fecap.firebaseio.com",
+  projectId: "icloc-fecap",
+  storageBucket: "icloc-fecap.appspot.com",
+  messagingSenderId: "340928006551",
+  appId: "1:340928006551:web:e6ce8ccb1a081dbe"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
